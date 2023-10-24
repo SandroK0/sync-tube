9723415fb1273c03738ab598579bcc55d587cd20
your_username <sandrok246@gmail.com>
2023-10-24 11:38:27 +0400

    up1

--- back(flask)/sync-tube-flask/venv/bin/activate.fish
+++ back(flask)/sync-tube-flask/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/sandro/Desktop/sync-tube-flask/venv"
+set -gx VIRTUAL_ENV "/home/sandro/Desktop/SyncTube/back(flask)/sync-tube-flask/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
